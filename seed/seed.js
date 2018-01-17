bd336357dffff74e5eed8d8fb21d106d3d82fd14
kenystev <kjestevez.0795@gmail.com>
2018-01-16 23:19:04 -0600

    [ADD] Route and Student seeds | throwing errors on seed

--- seed/seed.js
+++ seed/seed.js
@@ -1,14 +1,26 @@
 'use strict';
 var path = require('path');
 var users = require(path.resolve(__dirname, 'data/user/user_seed'));
+var routes = require(path.resolve(__dirname, 'data/route/route_seed'));
+var students = require(path.resolve(__dirname, 'data/student/student_seed'));
 
-var models = ['UserAccount', 'ACL', 'Role', 'RoleMapping', 'AccessToken'];
+var models = ['UserAccount', 'ACL', 'Role', 'RoleMapping', 'AccessToken', 'Student', 'Route'];
 
 module.exports = function(app, ds) {
     app.dataSources[ds].automigrate(models, function(err) {
         if (err)
             throw _err;
         app.models.UserAccount.create(users, function(_err) {
+            if (_err)
+                throw _err;
+
+        })
+        app.models.Route.create(routes, function(_err) {
+            if (_err)
+                throw _err;
+
+        })
+        app.models.Student.create(users, function(_err) {
             if (_err)
                 throw _err;
 
