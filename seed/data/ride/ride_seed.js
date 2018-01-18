0e9c8a79d4b8a1ab8af9280bc27e6831b58a7cb9
Jobarah <barahona.josue@unitec.edu>
2018-01-18 14:36:59 -0600

    [Update] route model - added relation with ride

--- seed/data/ride/ride_seed.js
+++ seed/data/ride/ride_seed.js
@@ -2,46 +2,56 @@
 
 module.exports = [
     {
+        routeId: 1,
         name: 'route_1',
         plate: 'AFCB133',
         date: new Date()
     }, {
+        routeId: 1,
         name: 'route_2',
         plate: 'AFCB133',
         date: new Date()
     },
     {
+        routeId: 1,
         name: 'route_3',
         plate: 'AFCB133',
         date: new Date()
     }, {
+        routeId: 1,
         name: 'route_4',
         plate: 'AFCB133',
         date: new Date()
     },
     {
+        routeId: 1,
         name: 'route_5',
         plate: 'AFCB133',
         date: new Date()
     }, {
+        routeId: 1,
         name: 'route_6',
         plate: 'AFCB133',
         date: new Date()
     },
     {
+        routeId: 1,
         name: 'route_7',
         plate: 'AFCB133',
         date: new Date()
     }, {
+        routeId: 1,
         name: 'route_8',
         plate: 'AFCB133',
         date: new Date()
     },
     {
+        routeId: 1,
         name: 'route_9',
         plate: 'AFCB133',
         date: new Date()
     }, {
+        routeId: 1,
         name: 'route_10',
         plate: 'AFCB133',
         date: new Date()
