6e775f5f07c0ae3cd48ff65aaf118101807227e8
Jobarah <barahona.josue@unitec.edu>
2018-01-17 21:19:45 -0600

    [Update] route_seed - key change

--- seed/data/route/route_seed.js
+++ seed/data/route/route_seed.js
@@ -2,38 +2,38 @@
 
 module.exports = [
     {
-        route: 'route_1',
+        name: 'route_1',
         description: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Curabitur sed'
     }, {
-        route: 'route_2',
+        name: 'route_2',
         description: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Curabitur sed'
     },
     {
-        route: 'route_3',
+        name: 'route_3',
         description: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Curabitur sed'
     }, {
-        route: 'route_4',
+        name: 'route_4',
         description: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Curabitur sed'
     },
     {
-        route: 'route_5',
+        name: 'route_5',
         description: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Curabitur sed'
     }, {
-        route: 'route_6',
+        name: 'route_6',
         description: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Curabitur sed'
     },
     {
-        route: 'route_7',
+        name: 'route_7',
         description: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Curabitur sed'
     }, {
-        route: 'route_8',
+        name: 'route_8',
         description: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Curabitur sed'
     },
     {
-        route: 'route_9',
+        name: 'route_9',
         description: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Curabitur sed'
     }, {
-        route: 'route_10',
+        name: 'route_10',
         description: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Curabitur sed'
     }
 ];
